45344a86b19a8ce76338c3654c17b91048eaae0e
nekx <nekxala@gmail.com>
2022-01-31 16:44:42 -0600

    Create nyt.user.js

---
+++ nyt.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        Advertise this you assholes
+// @namespace   Violentmonkey Scripts
+// @match       https://www.nytimes.com/2022/01/31/business/media/new-york-times-wordle.html
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 1/31/2022, 4:35:55 PM
+// ==/UserScript==
+
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+    removeStupidAdvertisingShit();
+  }
+}
+
+function removeStupidAdvertisingShit(){
+  // remove overlay containers
+  document.getElementById('gateway-content').remove();
+  document.getElementById('app').firstChild.firstChild.childNodes[2].remove();
+  
+  // enable scroll
+  document.getElementById('app').firstChild.childNodes[0].style.overflow = "scroll";
+}
